5a01ed539141535dbf1acf57338284e79c54fc05
Michael Rosenberg <michaelrosenberg@Michaels-MacBook-Pro.local>
2016-02-21 12:22:29 -0500

    Updated with new mapper material

--- scripts/tdsc_code.R
+++ scripts/tdsc_code.R
@@ -3,6 +3,9 @@
 
 #imports
 
+#standard constants
+pchVal = 19 #for most plots
+
 #load in dataset
 datasetString = (
 "http://www.stat.cmu.edu/tartandatasciencecup/20150708-citibike-tripdata.csv")
@@ -26,7 +29,12 @@ barplot(userTypeCounts, col = "Blue")
 #   manner
 #   -many more subscribers than one-off customers
 #map out based on longitude and latitude
-mapper <- function(z, levels, isStartStation, ...) {
+stationLevelFrame = data.frame(station = unique(bikeFrame$start.station.name),
+                             latitude = rep(
+                                0,length(unique(bikeFrame$start.station.name))),
+                             longitude = rep(
+                                0,length(unique(bikeFrame$start.station.name))))
+mapper <- function(z, levels, ...) {
     # Which quantiles do we need?
     probs <- seq(from=0, to=1, length.out=(levels+1))
     # What are those quantiles?
@@ -35,34 +43,34 @@ mapper <- function(z, levels, isStartStation, ...) {
     z.categories <- cut(z, z.quantiles, include.lowest=TRUE)
     # Make up a color scale
     shades <- terrain.colors(levels)
-    if (isStartStation){
-        #want to plot based on start station
-        plot(x=bikeFrame$start.station.longitude, 
-             y=tractFrame$start.station.latitude,col=shades[z.categories], ...)
-    }
-    else { #plot based on end station
-        #want to plot based on start station
-        plot(x=bikeFrame$end.station.longitude, 
-             y=tractFrame$end.station.latitude,col=shades[z.categories], ...)
-    }
+    #want to plot based on start station
+    plot(x=stationLevelFrame$longitude, 
+             y=stationLevelFrame$latitude,col=shades[z.categories], ...)
     invisible(list(quantiles=z.quantiles, categories=z.categories))
 }
 #then plot each latitude-longitude map based on desired variables to consider
-stationLevelFrame = data.frame(station = unique(bikeFrame$start.station.name),
-                            latitude = rep(
-                            0,length(unique(bikeFrame$start.station.name))),
-                            longitude = rep(
-                            0,length(unique(bikeFrame$start.station.name))))
+numLevels = 4 #just for friendliness for the visual of the map
 #get longitude and latitude pairs for each station
-for (i in 1:dim(stationFrame)[1]){
+for (i in 1:dim(stationLevelFrame)[1]){
     givenStation = stationLevelFrame$station[i]
-    givenStationObs = bikeFrame[
+    givenStationObs = bikeFrame[ #find station observations
                         which(bikeFrame$start.station.name == givenStation),]
-    stationLevelFrame$latitude = givenStationObs$start.station.latitude[1]
-    stationLevelFrame$longitude = givenStationObs$start.station.longitude[1]
+    stationLevelFrame$latitude[i] = givenStationObs$start.station.latitude[1]
+    stationLevelFrame$longitude[i] = givenStationObs$start.station.longitude[1]
 }
-
-
+#get num uses overall
+stationLevelFrame$numUses = rep(0,length(stationLevelFrame$station))
+for (i in 1:dim(stationLevelFrame)[1]){
+    givenStation = stationLevelFrame$station[i]
+    stationLevelFrame$numUses[i] = length(bikeFrame$start.station.name[which(
+                        bikeFrame$start.station.name == givenStation)])
+}
+#then plot this using the mapper
+numUses.map = mapper(stationLevelFrame$numUses,numLevels,pch = pchVal,
+                     xlab= "Longitude", ylab = "Latitude",
+                     main = "Num Uses on\nLongitude and Latitude")
+legend("bottomleft", legend=levels(numUses.map$categories), pch=pchVal,
+       col=terrain.colors(numLevels), cex=0.8)
 #then plot each latitude-longitude map based on desired variables to consider
 # Ian's code: the following code looks at the 
 # start-stop frequencies, and calculates the distance between them
